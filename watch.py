5e9928aa41334c3db9d67ad0942929750df1b938
Kevin Wong <1996kevinwong@gmail.com>
2019-04-02 17:34:34 -0400

    Create watch.py

---
+++ watch.py
@@ -0,0 +1,16 @@
+import tweepy
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline(count = 21)
+for tweet in public_tweets:
+    print(tweet.created_at)
