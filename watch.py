24ab32b2e10e40c1ba44da2c9f0d0419d2d7e4b3
Kevin <1996kevinwong@gmail.com>
2019-04-05 13:23:46 -0400

    Json
    Beginning to set up JSON interaction

--- watch.py
+++ watch.py
@@ -1,4 +1,5 @@
 import tweepy
+import json
 from auth import (
     consumer_key,
     consumer_secret,
@@ -6,15 +7,22 @@ from auth import (
     access_token_secret
 )
 
+#Load in search criteria 
+with open("criteria.json", "r") as read_file:
+    data = json.load(read_file)
+
+print(data.get("keywords")[0])
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline(count = 10)
-for tweet in public_tweets:
+public_tweets = api.home_timeline(count = 1)
+
+#for tweet in public_tweets:
 
     #Checks for tweets with links to specific websites
     #Improve it so that it can be from general domain
-    if(len(tweet.entities['urls']) >= 1):
-        print(tweet.entities['urls'][0]['expanded_url'])
+    #if(len(tweet.entities['urls']) >= 1):
+    #   print(tweet.entities['urls'][0]['expanded_url'])
